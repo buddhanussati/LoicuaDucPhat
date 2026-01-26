64ea40eefeb61cb8ac791ef865f1d2d7d4c5ae04
buddhanussati <buddhanussati@gmail.com>
2026-01-26 15:48:31 +0700

    Update sw.js

--- congcu/sw.js
+++ congcu/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hanhgia-app-v14';
+const CACHE_NAME = 'hanhgia-app-v15';
 // Danh sách các file cần lưu để chạy offline
 const ASSETS = [
   './',
@@ -46,6 +46,7 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
 
 
 
