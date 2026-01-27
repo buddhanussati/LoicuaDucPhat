e122bd66e4e0c01fd5fb993bead6405beb51faf9
buddhanussati <buddhanussati@gmail.com>
2026-01-27 16:08:41 +0700

    Add files via upload

--- congcu/sw.js
+++ congcu/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hanhgia-app-v17';
+const CACHE_NAME = 'hanhgia-app-v18';
 // Danh sách các file cần lưu để chạy offline
 const ASSETS = [
   './',
--- congcu/sw2.js
+++ congcu/sw2.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meditator-journal-app-v17';
+const CACHE_NAME = 'meditator-journal-app-v18';
 // Danh sách các file cần lưu để chạy offline
 const ASSETS = [
   './',
